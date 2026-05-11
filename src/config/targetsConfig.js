cd13b126bc97fd0178744a398bf41d156dd5bbed
Paritchayanon <140487319+Paritchaynon@users.noreply.github.com>
2026-05-11 17:50:31 +0700

    [add] Added new target and UI/UX

--- src/config/targetsConfig.js
+++ src/config/targetsConfig.js
@@ -4,7 +4,19 @@ export const targetsConfig = [
     id: "target_0",
     titleKey: "targets.target_0.title",
     descKey: "targets.target_0.desc"
-  }
+  },
+  {
+    index: 1,
+    id: "target_1",
+    titleKey: "targets.target_1.title",
+    descKey: "targets.target_1.desc"
+  },
+  {
+    index: 2,
+    id: "target_2",
+    titleKey: "targets.target_2.title",
+    descKey: "targets.target_2.desc"
+  },
   // You can easily add more targets here!
   // {
   //   index: 1,
